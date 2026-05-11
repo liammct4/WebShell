6aafc3695f70417d8f617bd815e7a2e5eed322a5
liammct4 <lmcrailt4@gmail.com>
2026-05-11 12:48:11 +0100

    Add config parsing.

---
+++ WebShell/Config/LoadWebAppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace WebShell.Config
+{
+	public class LoadWebAppConfig
+	{
+		public string CustomTitle { get; set; } = "WebShell";
+		public bool UseDocumentTitle { get; set; } = false;
+		public ServerSettings Server { get; set; } = new();
+	}
+}
---
+++ WebShell/Utilities/Network.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace WebShell.Utilities
+{
+	public static class Network
+	{
+		public static bool IsPortInUse(int port)
+		{
+			IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+			IPEndPoint[] endpoints = ipProperties.GetActiveTcpListeners();
+			return endpoints.Any(e => e.Port == port);
+		}
+	}
+}
